0834830d4148287cf6ee98edc08f3f872ad36e3e
Camilo Arias <andresithomcrisas@gmail.com>
2023-04-26 16:17:53 -0500

    Solving bug in fibonacci when 2 = 1

--- Modulo_4/fibonacci.py
+++ Modulo_4/fibonacci.py
@@ -1,5 +1,5 @@
 def fibonacci(n):
-    if n == 0 or n == 1:
+    if n == 0 or n == 1 or n == 2:
         return 1
     # Verificar funcionamiento del return
     # print(f'aqui: {fibonacci(n - 1) + fibonacci(n - 2)}')
