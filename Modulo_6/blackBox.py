f62c0e6ce00a6d64a261502cb5d58557fa804d28
Camilo Arias <andresithomcrisas@gmail.com>
2023-09-11 19:43:51 -0500

    black box testing

---
+++ Modulo_6/blackBox.py
@@ -0,0 +1,25 @@
+import unittest
+
+#Defined function what we go to testing
+def suma(num_1, num_2):
+    return num_1+num_2
+
+class BlackBoxTest(unittest.TestCase):
+    #test1
+    def test_suma_dos_positivos(self):
+        num_1 = 10
+        num_2 = 5
+
+        result = suma(num_1, num_2)
+
+        self.assertEqual(result, 15)
+    #test2
+    def test_suma_dos_negativos(self):
+        num_1 = -10
+        num_2 = -7
+
+        result = suma(num_1, num_2)
+        self.assertEqual(result, -17)
+
+if __name__ == '__main__':
+    unittest.main()
